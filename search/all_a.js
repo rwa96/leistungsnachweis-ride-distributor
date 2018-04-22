8f006230af4dfa9d38533cde1091540e68113df4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-22 14:16:56 +0000

    Deploy ob-algdatii-ss18/leistungsnachweis-ride-distributor to github.com/ob-algdatii-ss18/leistungsnachweis-ride-distributor.git:gh-pages

--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['operator_28_29',['operator()',['../classTensor.html#aa0f9ecd39e817deecbaef08f7a31898f',1,'Tensor::operator()(const int x)'],['../classTensor.html#a5115dcc8a7adb06f8442bc18d3c3f74c',1,'Tensor::operator()(const int x, const int y)'],['../classTensor.html#a789cc917e5c9d14d12bb4a788d429acd',1,'Tensor::operator()(const int x, const int y, const int z)']]],
-  ['outputdata',['OutputData',['../classOutputData.html',1,'OutputData'],['../classOutputData.html#aa70fb9ef9d074983236cdb5b7e16e436',1,'OutputData::OutputData()']]],
+  ['operator_28_29',['operator()',['../classTensor.html#ac3f576717cb3532421eb6074ae390693',1,'Tensor::operator()(const int i)'],['../classTensor.html#a8ad07f10842e74620f50fad67df8ca52',1,'Tensor::operator()(const int row, const int col)'],['../classTensor.html#a0957d4a29bf9b530c7998ff3453bb5b6',1,'Tensor::operator()(const int row, const int col, const int offset)']]],
+  ['outputdata',['OutputData',['../classOutputData.html',1,'OutputData'],['../classOutputData.html#a3d95c4111813dbbb72cbc7fa0243e6c9',1,'OutputData::OutputData(std::unique_ptr&lt; Tensor&lt; unsigned &gt;&gt; &amp;value)'],['../classOutputData.html#ae159920f2d5c6012a13d69af2c264109',1,'OutputData::OutputData(std::shared_ptr&lt; OutputData &gt; &amp;parent, std::unique_ptr&lt; Tensor&lt; unsigned &gt;&gt; &amp;value)']]],
   ['outputdatatest',['OutputDataTest',['../classOutputDataTest.html',1,'']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['tensor',['Tensor',['../classTensor.html#ae966f3807d5bfb78c049cf8c494e16ba',1,'Tensor::Tensor()'],['../classTensor.html#adf0e07f9cd7b6d3af89ace481eead0df',1,'Tensor::Tensor(const std::vector&lt; unsigned &gt; dims)'],['../classTensor.html#ad34719ebad82abc3a89903f46bc905d6',1,'Tensor::Tensor(Tensor&lt; T &gt; &amp;other)']]]
+  ['tensor',['Tensor',['../classTensor.html#ae966f3807d5bfb78c049cf8c494e16ba',1,'Tensor::Tensor()'],['../classTensor.html#adf0e07f9cd7b6d3af89ace481eead0df',1,'Tensor::Tensor(const std::vector&lt; unsigned &gt; dims)'],['../classTensor.html#ae7572da1ffa12abc71e60e4b8f5253cc',1,'Tensor::Tensor(const std::vector&lt; unsigned &gt; dims, const std::vector&lt; T &gt; data)'],['../classTensor.html#ad34719ebad82abc3a89903f46bc905d6',1,'Tensor::Tensor(Tensor&lt; T &gt; &amp;other)']]]
 ];
