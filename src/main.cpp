f07d8e3c97df930a707a68f12a8eb6d29bcba59a
David Askari <askari@mailbox.org>
2018-05-03 19:22:12 +0200

    Update main.

--- src/main.cpp
+++ src/main.cpp
@@ -35,79 +35,86 @@ bool checkCarTime(Types::CarData carData, unsigned T)
 
 int main(int argc, char *argv[])
 {
-	if (argc == 6)
+	if (argc != 6)
 	{
-
-		// InputData
-		std::shared_ptr<InputData> inputData;
-
-		try
-		{
-			inputData = InputData::genFromFile(argv[1]);
-		}
-		catch (const std::fstream::failure e)
-		{
-			std::cerr << e.what() << "(Invalid file or path)" << std::endl;
-			return 1;
-		}
-
-		unsigned nTries = atoi(argv[3]);
-		unsigned kBest = atoi(argv[4]);
-		unsigned beamSize = atoi(argv[5]);
-
-		// Generator + first Generator run
-		Generator generator(*inputData, nTries, kBest);
-		Types::Choices choices;
-		std::vector<int> unassigned(inputData->nRides);
-		std::iota(unassigned.begin(), unassigned.end(), 0);
-		std::unique_ptr<Types::CarData> cars(new Types::CarData(inputData->fleetSize));
-		std::shared_ptr<SearchGraphNode> parentNode;
-		generator.generate(choices, parentNode, unassigned, std::move(cars));
-
-		// Aggregator + first Aggregator run
-		Aggregator aggregator(*inputData, beamSize);
-		aggregator.aggregate(choices);
-
-		// Main while loop, which finds best choice.
-		// Runs until there a no more choices with unassigned rides or all choices are over the simulation time.
-		Types::Choices newChoices;
-
-		while (true)
-		{
-
-			for (std::unique_ptr<Types::Choice> &choice : choices)
-			{
-
-				if (choice->unassigned.size() > 0 && checkCarTime(choice->cars, inputData->maxTime))
-				{
-					std::unique_ptr<Types::CarData> newCars(new Types::CarData(choice->cars));
-					generator.generate(newChoices, choice->searchGraphNode, choice->unassigned, std::move(newCars));
-				}
-			}
-
-			if (newChoices.size() > 0)
-			{
-
-				aggregator.aggregate(newChoices);
-				choices = std::move(newChoices);
-				newChoices.clear();
-				std::cout << (inputData->nRides - choices.front()->unassigned.size()) * 100.0 / inputData->nRides << "%" << std::endl;
-			}
-			else
-			{
-
-				std::cout << "No more choices to aggregate." << std::endl;
-				std::cout << "Printing best choice:" << std::endl;
-				printChoice(choices.front());
-				choices.front()->searchGraphNode->writeToFile(argv[2], inputData->fleetSize);
-				break;
-			}
-		}
-	}
-	else
-	{
-		std::cerr << "No input and output path argument set." << std::endl;
+		std::cerr << "No input path, output path, nTries, kBest and beamSize argument set." << std::endl;
 		return 1;
 	}
+
+	unsigned nTries = atoi(argv[3]);
+	unsigned kBest = atoi(argv[4]);
+	unsigned beamSize = atoi(argv[5]);
+
+	// InputData
+	std::shared_ptr<InputData> inputData;
+	try
+	{
+		inputData = InputData::genFromFile(argv[1]);
+	}
+	catch (const std::fstream::failure e)
+	{
+		std::cerr << e.what() << "(Invalid file or path)" << std::endl;
+		return 1;
+	}
+
+	// Setup Generator and requirements, then do first Generator run
+	Generator generator(*inputData, nTries, kBest);
+
+	Types::Choices choices;
+	std::vector<int> unassigned(inputData->nRides);
+	std::iota(unassigned.begin(), unassigned.end(), 0);
+	std::unique_ptr<Types::CarData> cars(new Types::CarData(inputData->fleetSize));
+	std::shared_ptr<SearchGraphNode> parentNode;
+
+	generator.generate(choices, parentNode, unassigned, std::move(cars));
+
+	// Setup Aggregator, then do first Aggregator run
+	Aggregator aggregator(*inputData, beamSize);
+	aggregator.aggregate(choices);
+
+	// Main while loop, which finds best choice.
+	// Runs until there a no more choices with unassigned rides or all choices are over the simulation time.
+	Types::Choices newChoices;
+
+	while (true)
+	{
+		// Generator
+		for (std::unique_ptr<Types::Choice> &choice : choices)
+		{
+			// If valid choice exists, generate new choices and save in newChoices
+			if (choice->unassigned.size() > 0 && checkCarTime(choice->cars, inputData->maxTime))
+			{
+				std::unique_ptr<Types::CarData> newCars(new Types::CarData(choice->cars));
+				generator.generate(newChoices, choice->searchGraphNode, choice->unassigned, std::move(newCars));
+			}
+		}
+
+		// Aggregator
+		// If new choices exists, aggregate those. Otherwise stop this while loop.
+		if (newChoices.size() > 0)
+		{
+			aggregator.aggregate(newChoices);
+			choices = std::move(newChoices);
+			newChoices.clear();
+			std::cout << (inputData->nRides - choices.front()->unassigned.size()) * 100.0 / inputData->nRides << "%" << std::endl;
+		}
+		else
+		{
+			std::cout << "No more choices to aggregate." << std::endl;
+			std::cout << "Printing best choice:" << std::endl;
+			printChoice(choices.front());
+
+			unsigned score = 0;
+			for (int i = 0; i < inputData->fleetSize; ++i) {
+				score += choices.front()->cars.p(i);
+			}
+			std::cout << "Google score: " << score << std::endl;
+
+			std::cout << "Writing output file.." << std::endl;
+			choices.front()->searchGraphNode->writeToFile(argv[2], inputData->fleetSize);
+			break;
+		}
+	}
+
 	return 0;
 }
