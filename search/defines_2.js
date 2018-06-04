9dacf237647eb991459be9685de26c5c10efca82
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-04 23:48:29 +0000

    Deploy ob-algdatii-ss18/leistungsnachweis-ride-distributor to github.com/ob-algdatii-ss18/leistungsnachweis-ride-distributor.git:gh-pages

--- search/defines_2.js
+++ search/defines_2.js
@@ -3,5 +3,7 @@ var searchData=
   ['dbg_5fprint_5f1d_5ftensor',['DBG_PRINT_1D_TENSOR',['../Tensor_8hpp.html#a06a8b7c04744f5abadc2e9f9cc24f6e3',1,'Tensor.hpp']]],
   ['dbg_5fprint_5f2d_5ftensor',['DBG_PRINT_2D_TENSOR',['../Tensor_8hpp.html#a79a5345abb5b8e698633c09df613b177',1,'Tensor.hpp']]],
   ['dbg_5fprint_5fchoice',['DBG_PRINT_CHOICE',['../Algorithm_8cpp.html#abfe7b45fe480552151a7e902c7ed6993',1,'Algorithm.cpp']]],
+  ['dbg_5fprint_5fjv_5fcostmat',['DBG_PRINT_JV_COSTMAT',['../RLAPSolverJV_8cpp.html#a46028c7f413a7c61fb9d4ea5b36ca99c',1,'RLAPSolverJV.cpp']]],
+  ['dbg_5fprint_5fjv_5fsol',['DBG_PRINT_JV_SOL',['../RLAPSolverJV_8cpp.html#a9b6dbf7efe527d8d2ebd209b0cfcc013',1,'RLAPSolverJV.cpp']]],
   ['dbg_5fprint_5fzeros',['DBG_PRINT_ZEROS',['../RLAPSolverHungarian_8cpp.html#aed02384de9b37759252a45d57863fd8d',1,'RLAPSolverHungarian.cpp']]]
 ];
